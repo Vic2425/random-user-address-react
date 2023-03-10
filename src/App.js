6caabf1aab211ea0cd5103ebbb5aa15451ed5387
Vic <vicjs612@gmail.com>
2023-03-09 20:47:06 -0600

    fetach and render data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 const [user, setUser] = useState([]);
+const [isHidden, setHide] = useState(true)
+
+// const handleClick = (index) => {
+//   let status 
+// }
 
 useEffect(() => {
-  axios.get('https://randomuser.me/api?results=5')
+  fetch('https://randomuser.me/api?results=5')
     .then(res => {
-      // console.log(res)
-      setUser(res.data.result)
+      return res.json()
     })
-    .catch(err => {
-      console.log(err)
-    })
-})
+    .then(user => (setUser(user.results)))
+  }, []);    
 
   return (
     <div className="App">
       <header className="App-header">
         <div>
           {user.map((user, index) => {
-            return <li key={index}>{user.name.first}{user.name.last}</li>
+            return <div>
+              <img src={user.picture.medium} />
+              <li key={index}>
+                <h4>{user.name.first}{user.name.last}</h4> 
+                <p>Gender: {user.gender}</p>
+                {isHidden ? 
+                  <div></div> : 
+                    <div>
+                      <p>Age: {user.dob.age}</p>
+                      <p>Email: {user.email}</p>
+                      <p>City: {user.location.city}</p>
+                    </div>
+                }
+                <button onClick={(index) => {isHidden ? setHide(false) : setHide(true)}}>Show more info</button>
+              </li>
+            </div>
           })}
         </div>
       </header>
